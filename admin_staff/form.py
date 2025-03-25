8020e34d601bc1e15ab7d314366b0191c59005a6
William <robertfraga517@gmail.com>
2025-03-25 22:04:11 +0800

    small changes

--- admin_staff/form.py
+++ admin_staff/form.py
@@ -1,15 +1,23 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Announcement, StudentProfile, FacultyStaff
+
 
 class announcementForm(ModelForm):
     class Meta:
         model = Announcement
-        fields = '__all__'
+        fields = ['title', 'event', 'body']
+        widgets = {
+            'event': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
 
 class studentForm(ModelForm):
     class Meta:
         model = StudentProfile
         fields = '__all__'
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
 
 class facultyForm(ModelForm):
     class Meta:
